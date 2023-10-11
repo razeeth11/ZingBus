fe5a21895d9f78abb4c816ee40c479740d485af9
razeeth11 <razeeth011@gmail.com>
2023-10-11 15:05:42 +0530

    Lounges

--- src/ZingBusDiv.jsx
+++ src/ZingBusDiv.jsx
@@ -61,6 +61,50 @@ export function ZingBusDiv() {
           </div>
         </div>
       </div>
+      <div className="zing-bus-lounge">
+        <div className="zing-bus-lounge">
+          <h1 className="zing-bus-lounge-head">Zingbus Premium Lounges</h1>
+          <h2 className="zing-bus-lounge-sub-head">
+            Rest, Refresh, rejuvenate
+          </h2>
+          <hr />
+          <div className="zing-bus-lounge-image">
+            <div className="zing-img">
+              <img
+                src="https://d1flzashw70bti.cloudfront.net/next-gen/images/lounges/gorakhpurEntrance-thumb.webp"
+                alt=""
+                width='280px'
+              />
+              <div className="zing-lounge-content">
+                  <span class="material-symbols-outlined">location_on</span>
+                  <span>Ahmedabad</span>
+              </div>
+            </div>
+            <div className="zing-img">
+              <img
+                src="https://d1flzashw70bti.cloudfront.net/next-gen/images/lounges/WL-Manali-1-thumb.webp"
+                alt=""
+                width='280px'
+              />
+              <div className="zing-lounge-content">
+                  <span class="material-symbols-outlined">location_on</span>
+                  <span>Manali</span>
+              </div>
+            </div>
+            <div className="zing-img">
+              <img
+                src="https://d1flzashw70bti.cloudfront.net/next-gen/images/lounges/WL-Lucknow-thumb.webp"
+                alt=""
+                width='280px'
+              />
+              <div className="zing-lounge-content">
+                  <span class="material-symbols-outlined">location_on</span>
+                  <span>Lucknow</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
