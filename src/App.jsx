a9069a7800eaf11a16e715172ecfa2361dc7cf11
razeeth11 <razeeth011@gmail.com>
2023-10-11 11:40:25 +0530

    Main-Div

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { MainDiv } from "./MainDiv";
 import { NavBar } from "./NavBar";
 
 function App() {
   return (
     <>
       <NavBar />
+      <MainDiv />
     </>
   );
 }
---
+++ src/MainDiv.jsx
@@ -0,0 +1,39 @@
+export function MainDiv() {
+  return (
+    <div className="main-div">
+      <div className="main-image-div">{/* main-image-CSS */}</div>
+      <div className="main-search-comp">
+        <div className="from-comp">
+          <div className="from">
+            <span class="material-symbols-outlined">location_on</span>
+            <p>From City</p>
+          </div>
+          <div className="from-input">
+            <input type="text" />
+          </div>
+        </div>
+        <div className="to-comp">
+          <div className="to">
+            <span class="material-symbols-outlined">location_on</span>
+            <p>To City</p>
+          </div>
+          <div className="To-input">
+            <input type="text" />
+          </div>
+        </div>
+        <div className="date-comp">
+          <div className="date">
+            <span class="material-symbols-outlined">calendar_month</span>
+            <p>Date</p>
+          </div>
+          <div className="date-input">
+            <input type="text" />
+          </div>
+        </div>
+        <div className="search-comp">
+          <button>Search</button>
+        </div>
+      </div>
+    </div>
+  );
+}
